f139ed85d464fe87886aa743b2a79043a6751be7
Louis SERRANO <SERRANO@GALLIFA11>
2025-10-23 08:54:14 +0200

    feat(db): Add dockerization of database

--- db/init/01-init-database.sql
+++
@@ -1,8 +0,0 @@
--- Initialize the image_detection database
-CREATE DATABASE IF NOT EXISTS image_detection;
-USE image_detection;
-
--- Create user for the application
-CREATE USER IF NOT EXISTS 'app_user'@'%' IDENTIFIED BY 'app_password';
-GRANT ALL PRIVILEGES ON image_detection.* TO 'app_user'@'%';
-FLUSH PRIVILEGES;
--- db/init/init.sql
+++ db/init/init.sql
@@ -1,3 +1,12 @@
+-- Initialize the image_detection database
+CREATE DATABASE IF NOT EXISTS image_detection;
+USE image_detection;
+
+-- Create user for the application
+CREATE USER IF NOT EXISTS 'app_user'@'%' IDENTIFIED BY 'app_password';
+GRANT ALL PRIVILEGES ON image_detection.* TO 'app_user'@'%';
+FLUSH PRIVILEGES;
+
 -- Create tables for the image detection application
 USE image_detection;
 
